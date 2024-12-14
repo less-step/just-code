43514b73647548255827cdb956e1ef572eb982df
姜鑫 <1599925910@qq.com>
2024-12-14 20:59:13 +0800

    提交(删除多余的错误处理)

--- packages/cli/scripts/build.mjs
+++ packages/cli/scripts/build.mjs
@@ -3,24 +3,18 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import { rollup } from "rollup";
 async function build() {
-	try {
-		// 动态配置 Rollup
-		const bundle = await rollup({
-			input: "lib/index.ts", // 输入文件路径
-			plugins: [typescript(), resolve(), commonjs()],
-			external: [/^@only-code\/.*/],
-		});
-		// 输出配置
-		await bundle.write({
-			file: "bin/index.js", // 输出文件路径
-			format: "cjs", // 输出格式
-			sourcemap: true, // 是否生成 sourcemap
-		});
-
-		console.log("打包成功!");
-	} catch (error) {
-		console.error("打包失败，错误:", error);
-	}
+	// 动态配置 Rollup
+	const bundle = await rollup({
+		input: "lib/index.ts", // 输入文件路径
+		plugins: [typescript(), resolve(), commonjs()],
+		external: [/^@only-code\/.*/],
+	});
+	// 输出配置
+	await bundle.write({
+		file: "bin/index.js", // 输出文件路径
+		format: "cjs", // 输出格式
+		sourcemap: true, // 是否生成 sourcemap
+	});
 }
 
 // 执行打包任务
--- scripts/build.mjs
+++ scripts/build.mjs
@@ -3,29 +3,24 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import { rollup } from "rollup";
 async function build() {
-	try {
-		// 动态配置 Rollup
-		const bundle = await rollup({
-			input: "lib/index.ts", // 输入文件路径
-			plugins: [resolve(), commonjs(), typescript()],
-			external: [/^@only-code\/.*/],
-		});
-		// 输出配置
-		await bundle.write({
-			file: "cjs/index.js", // 输出文件路径
-			format: "cjs", // 输出格式
-			sourcemap: true, // 是否生成 sourcemap
-		});
-		// 输出配置
-		await bundle.write({
-			file: "esm/index.js", // 输出文件路径
-			format: "cjs", // 输出格式
-			sourcemap: true, // 是否生成 sourcemap
-		});
-		console.log("打包成功!");
-	} catch (error) {
-		console.error("打包失败，错误:", error);
-	}
+	// 动态配置 Rollup
+	const bundle = await rollup({
+		input: "lib/index.ts", // 输入文件路径
+		plugins: [resolve(), commonjs(), typescript()],
+		external: [/^@only-code\/.*/],
+	});
+	// 输出配置
+	await bundle.write({
+		file: "cjs/index.js", // 输出文件路径
+		format: "cjs", // 输出格式
+		sourcemap: true, // 是否生成 sourcemap
+	});
+	// 输出配置
+	await bundle.write({
+		file: "esm/index.js", // 输出文件路径
+		format: "cjs", // 输出格式
+		sourcemap: true, // 是否生成 sourcemap
+	});
 }
 
 // 执行打包任务
